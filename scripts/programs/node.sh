34484dae6a90bc49f1987d1ce796188d4c4ee843
Jose Clemente <jclementedev@gmail.com>
2023-12-28 15:37:27 -0500

    feat: add node & commitlint installation

--- scripts/programs/node.sh
+++ scripts/programs/node.sh
@@ -5,6 +5,19 @@
 # sudo apt install npm           # in many cases, npm is shipped with node
 # sudo npm install --global yarn # optional
 
+# =======================
+# Install Node with fnm
+# =======================
 curl -fsSL https://fnm.vercel.app/install | bash
 
 fnm install 18.19.0
+
+# =======================
+# Install commitlint
+# =======================
+npm install -g @commitlint/cli @commitlint/config-conventional
+
+echo "module.exports = {extends: ['@commitlint/config-conventional']}" > commitlint.config.js
+
+# Test commitlint
+echo 'foo: bar' | commitlint
